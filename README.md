120e6b41dbd83e2d85c4680963f59c30deb872d2
bergren2 <dan@heymrbass.com>
2017-12-19 20:57:09 -0600

    add modifications

--- README.md
+++ README.md
@@ -4,3 +4,25 @@ Summary
 Implementing autocomplete with Redis, NodeJS and jQuery:
 
 http://www.codeproject.com/Articles/853819/AutoComplete-with-Redis-NodeJS-and-jQuery
+
+## Dan's Notes
+
+Install things:
+
+    $ brew install redis
+
+Run Redis:
+
+    $ redis-server /usr/local/etc/redis.conf
+
+Populate Redis database:
+
+    $ ruby build_data_redis.rb data/icd9.txt icd9
+    $ ruby build_data_redis.rb data/icd9p.txt icd9p
+
+Run Node server (w/ Redis still running locally):
+
+    $ node node_app.js
+
+Go to http://127.0.0.1:1337/index.html and type in some characters for
+autocomplete!
--- node_app.js
+++ node_app.js
@@ -1,5 +1,10 @@
+// var redis = require("redis"), client = redis.createClient("port", "host", {
+//     'auth_pass': 'password',
+//     'return_buffers': true
+//   });
 var redis = require("redis"), client = redis.createClient();
 var http = require('http');
+var fs = require("fs");
 var ip="127.0.0.1";
 var port=1337;
 
@@ -14,6 +19,27 @@ var parseQueryString = function( queryString ) {
 };
 
 http.createServer(function (req, res) {
+  var filename;
+  console.log(req.url);
+  if (req.url.indexOf("index.html") >= 0) {
+    filename = "index.html";
+  } else if (req.url.indexOf("autocomplete.js") >= 0) {
+    filename = "autocomplete.js";
+  }
+  if (filename) {
+    console.log(filename);
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        res.writeHead(500, {"Content-Type": "text/plain"});
+        res.write(err + "\n");
+        res.end();
+        return;
+      }
+      res.writeHead(200);
+      res.write(file, "binary");
+      res.end();
+    });
+  }
   var q = parseQueryString(req.url.substring(req.url.indexOf('?') + 1));
   if (typeof(q["q"]) != 'undefined'){
 	  var query = decodeURIComponent(decodeURI(q["q"])).toLowerCase();
